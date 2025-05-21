a0deca6597cc2744cf9ad493c10fe7136591ac8e
LordPedroFelipe <pf.souza15@gmail.com>
2025-05-21 18:39:52 -0300

    feat: New build

--- src/app/models/apod.model.ts
+++ src/app/models/apod.model.ts
@@ -2,7 +2,7 @@ export class Apod {
     date!: string;
     explanation!: string;
     hdurl?: string;
-    media_type!: 'image' | 'video';
+    media_type!: 'image' | 'video' | 'other';
     service_version!: string;
     title!: string;
     url!: string;
