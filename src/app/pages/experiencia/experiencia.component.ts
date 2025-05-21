b92e97b0c126f13d39813e81785bca4a8f7210c5
LordPedroFelipe <pf.souza15@gmail.com>
2025-05-21 18:56:04 -0300

    feat: New adjustment on expirence

--- src/app/pages/experiencia/experiencia.component.ts
+++ src/app/pages/experiencia/experiencia.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
   styleUrls: ['./experiencia.component.scss']
 })
-export class ExperienciaComponent {
+export class ExperienciaComponent implements AfterViewInit {
+  @ViewChild('experienceSection', { static: false }) experienceSection!: ElementRef;
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      const cards = document.querySelectorAll('.observed-card');
+  
+      const cardObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+            cardObserver.unobserve(entry.target); // desativa após animar
+          }
+        });
+      }, { threshold: 0.3 });
+  
+      cards.forEach(card => cardObserver.observe(card));
+    }, 0);
+  }
+  
 }
