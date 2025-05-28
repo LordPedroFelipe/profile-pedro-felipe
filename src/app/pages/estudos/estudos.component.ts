539f0cd3acdd9b045fcc19178d8a5fdc956d2665
LordPedroFelipe <pf.souza15@gmail.com>
2025-05-28 14:39:01 -0300

    feat: adjustments to the animations on cards

--- src/app/pages/estudos/estudos.component.ts
+++ src/app/pages/estudos/estudos.component.ts
@@ -1,4 +1,6 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 import { AnimationObserverService } from 'src/app/services/animation-observer.service';
 
 @Component({
@@ -6,10 +8,27 @@ import { AnimationObserverService } from 'src/app/services/animation-observer.se
   templateUrl: './estudos.component.html',
   styleUrls: ['./estudos.component.scss']
 })
-export class EstudosComponent implements AfterViewInit {
-  constructor(private animationService: AnimationObserverService) {}
+export class EstudosComponent implements AfterViewInit, OnDestroy {
+  private langChangeSub!: Subscription;
+
+  constructor(
+    private animationService: AnimationObserverService,
+    private translateService: TranslateService
+  ) {}
 
   ngAfterViewInit(): void {
-    setTimeout(() => this.animationService.observarAnimacaoCards(), 0);
+    // Primeira chamada após renderizar o componente
+    setTimeout(() => this.animationService.observarAnimacaoCards(), 100);
+
+    // Nova chamada sempre que trocar idioma
+    this.langChangeSub = this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      setTimeout(() => this.animationService.observarAnimacaoCards(), 100);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.langChangeSub) {
+      this.langChangeSub.unsubscribe();
+    }
   }
 }
